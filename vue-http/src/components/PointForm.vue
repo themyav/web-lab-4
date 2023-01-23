8b85a017dd70d5508448b8bd17a2fda491f357a7
themyav <Myavochka119@gmail.com>
2023-01-24 02:37:48 +0300

    Changed radio button design

--- vue-http/src/components/PointForm.vue
+++ vue-http/src/components/PointForm.vue
@@ -2,13 +2,26 @@
   <div>
     <canvas v-on:click="processGraphClick" id="graph" height="300" width="300"></canvas>
     <label for="x">Выберите X: </label>
-    <input type="radio" name="x" value="1" v-model="x"> 1
-    <input type="radio" name="x" value="2" v-model="x"> 2<br>
+    <span><input type="radio" name="x" value="-5" v-model="x">-5</span>
+    <span><input type="radio" name="x" value="-4" v-model="x"> -4</span>
+    <span><input type="radio" name="x" value="-3" v-model="x"> -3</span>
+    <span><input type="radio" name="x" value="-2" v-model="x"> -2</span>
+    <span><input type="radio" name="x" value="-1" v-model="x"> -1</span>
+    <span><input type="radio" name="x" value="0" v-model="x"> 0</span>
+    <span><input type="radio" name="x" value="1" v-model="x"> 1</span>
+    <span><input type="radio" name="x" value="2" v-model="x"> 2</span>
+    <span><input type="radio" name="x" value="3" v-model="x"> 3</span>
+
+
     <label htmlFor="y">Введите значение Y</label> <input type="text" id="y" v-model="y"/> <br>
     <label for="r">Выберите R: </label>
-    <input type="radio" name="r" value="1" v-model="r" @change="changeR"> 1
-    <input type="radio" name="r" value="2" v-model="r" @change="changeR"> 2<br>
-    <input type="button" value="Отправить!" @click="validate"/>
+    <span><input type="radio" name="r" value="1" v-model="r" @change="changeR"> 1</span>
+    <span><input type="radio" name="r" value="1" v-model="r" @change="changeR"> 1</span>
+    <span><input type="radio" name="r" value="1" v-model="r" @change="changeR"> 1</span>
+    <span><input type="radio" name="r" value="1" v-model="r" @change="changeR"> 1</span>
+    <span><input type="radio" name="r" value="2" v-model="r" @change="changeR"> 2</span>
+    <span><input type="radio" name="r" value="3" v-model="r" @change="changeR"> 3</span><br>
+    <button @click="validate">Отправить!</button>
     <form action="/">
       <button>Вернуться на главную</button>
     </form>
@@ -21,7 +34,7 @@ import drawer from "@/js/drawer";
 
 export default {
   props: ['points', 'token', 'user'],
-  mixins : [drawer],
+  mixins: [drawer],
   data: function () {
     return {
       x: 0,
@@ -29,7 +42,7 @@ export default {
       r: 0,
       text: '',
       id: '',
-      refreshed : false
+      refreshed: false
     }
   },
   mounted() {
@@ -56,7 +69,7 @@ export default {
       this.save();
     },
     //отправка запроса для точки
-    save: function (currentToken=this.token) {
+    save: function (currentToken = this.token) {
       //console.log("current token is : ", currentToken);
       //передаем токен, чтобы он при необходимости был обновленный
       //this.token = currentToken;
@@ -77,12 +90,12 @@ export default {
             }
           }
       ).then(result => {
-          //console.log("put " + result.data + " to points")
-          //console.log(result);
-          this.$emit('onPointAdd', result.data);
-        }, () => {
+        //console.log("put " + result.data + " to points")
+        //console.log(result);
+        this.$emit('onPointAdd', result.data);
+      }, () => {
         console.log("error! token " + currentToken + " is invalid");
-        if(!this.refreshed){
+        if (!this.refreshed) {
           this.refreshed = true;
           this.$emit('refreshEvent');
         }
@@ -93,12 +106,23 @@ export default {
 </script>
 
 <style scoped>
-div{
+div {
   background-color: #fff001;
   border-radius: 5px;
 }
-canvas{
+
+canvas {
   border-radius: 5px;
   margin: 10px;
+}
+span{
+  margin: 0;
+  font-weight: bold;
+  color: #020530;
+}
+input[type="radio"]{
+  margin: 0 0 0 10px;
+  vertical-align: bottom;
+  height: 25px;
 }
 </style>
